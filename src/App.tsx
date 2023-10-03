278d5929cf20e2f09f218fccda99481a90c3d11b
cgathua <114153640+cgathua@users.noreply.github.com>
2023-10-03 10:02:31 -0400

    create: benefits section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import Navbar from "./Navbar/Navbar"
 import './Navbar/Navbar.css'
 import Home from "./Home/Home"
 import './Home/Home.css'
+import Benefits from "./Benefits/Benefits"
+import './Benefits/Benefits.css'
 
 
 function App(): JSX.Element {
   return (
     <>
       <Navbar />
-      < Home/>
+      <Home />
+      <Benefits />
     </>
   )
 }
