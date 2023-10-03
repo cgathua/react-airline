9b96dfaf495dd90fb242f289b8cb34ddf2a927ac
cgathua <114153640+cgathua@users.noreply.github.com>
2023-10-03 04:10:13 -0400

    create: Home Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./Navbar/Navbar"
 import './Navbar/Navbar.css'
+import Home from "./Home/Home"
+import './Home/Home.css'
+
 
 function App(): JSX.Element {
   return (
     <>
       <Navbar />
+      < Home/>
     </>
   )
 }
---
+++ src/Home/Home.tsx
@@ -0,0 +1,31 @@
+import SmallImg from '/images/2559290_1053(480w).webp'
+import MediumImg from '/images/2559290_1053(960w).webp'
+import LargeImg from '/images/2559290_1053(1920).webp'
+import DefaultImg from '/images/2559290_1053(1920).jpg'
+
+function Home(): JSX.Element {
+    return (
+        <>
+            <section id="home">
+                <div className="container">
+                    <img
+                        srcSet={`${SmallImg} 480w, ${MediumImg} 960w, ${LargeImg} 1920w, ${DefaultImg} 1920w`}
+                        src={DefaultImg}
+                        alt="airplane" 
+                        width={1920}
+                        height={1080}
+                        className='HomeImage'
+                    />
+                    <article className="HomeArticle">
+                        <h1>Affordable Regional Flights</h1>
+                        <p>We offer daily flights from Hartesfield-Jackson Atlanta International Airport, to Miami International Airport.</p>
+                        <p>Trusted by thousands of passengers since 2002.</p>
+                        <a className='btn' href="#booking">Book Now</a>
+                    </article>
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default Home
